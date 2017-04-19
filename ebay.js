ae2a2e705aa77bf21ffbfb0e1a03dd591b0d92c4
Jon Avery <jon.avery92@gmail.com>
2017-04-19 16:42:37 -0700

    Create ebay.js

---
+++ ebay.js
@@ -0,0 +1,22 @@
+function pullWork() {
+  /*
+  This function pulls all Ebays from the Work page into the Ebay page.
+  Only tested items are taken.
+  */
+  
+  // Cache ID's for sheets to be accessed.
+  var ebayID = "1EIDj7gxkk_QXf10ddvX70-756Ah7W395MRPQq0Q84Zw";
+  var workID = "1okDFF9236lGc4vU6W7HOD8D-3ak8e_zntehvFatYxnI";
+  var liquidID = "1Xqsc6Qe_hxrWN8wRd_vgdBdrCtJXVlvVC9w53XJ0BUM";
+}
+
+function pushReturns() {
+  /*
+  This function pushes returned Ebay items to the Liquidation page.
+  */
+  
+  // Cache ID's for sheets to be accessed.
+  var ebayID = "1EIDj7gxkk_QXf10ddvX70-756Ah7W395MRPQq0Q84Zw";
+  var workID = "1okDFF9236lGc4vU6W7HOD8D-3ak8e_zntehvFatYxnI";
+  var liquidID = "1Xqsc6Qe_hxrWN8wRd_vgdBdrCtJXVlvVC9w53XJ0BUM";
+}
