a951ea2038df32a882f5ed19d5b37e04a6716043
Jon Avery <jon.avery92@gmail.com>
2017-04-21 14:09:32 -0700

    Create test version of ebay.js

---
+++ ebay.js
@@ -0,0 +1,22 @@
+function pullWork() {
+  /*
+  This function pulls all Ebays from the Work page into the Ebay page.
+  Only tested items are taken.
+  */
+  
+  // Cache ID's for sheets to be accessed.
+  var ebayID = "1nbEos0-LK6lmr50FJHM7T0vPOt1hNuxqVfHGaYqy_zc";
+  var workID = "1w28MV69JaR99e2m-2hveMLDY_Ukbz1Meg-9RQK0-sik";
+  var liquidID = "1Vfzm-MogYyJB88YFmA4oWQ1M4bpT4Onw-ZGeFSpqseo";
+}
+
+function pushReturns() {
+  /*
+  This function pushes returned Ebay items to the Liquidation page.
+  */
+  
+  // Cache ID's for sheets to be accessed.
+  var ebayID = "1nbEos0-LK6lmr50FJHM7T0vPOt1hNuxqVfHGaYqy_zc";
+  var workID = "1w28MV69JaR99e2m-2hveMLDY_Ukbz1Meg-9RQK0-sik";
+  var liquidID = "1Vfzm-MogYyJB88YFmA4oWQ1M4bpT4Onw-ZGeFSpqseo";
+}
