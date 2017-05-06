4049b01d031b07e9b1fe9e6a8015740559c2382d
Jon Avery <jon.avery92@gmail.com>
2017-05-05 18:01:52 -0700

    Finalize the VLOOKUP updater

--- liqorders1.js
+++ liqorders1.js
@@ -33,8 +33,8 @@ function priceSearch() {
   Logger.log("Item Count: " + itemCount)
   
   // Copy formulas from Subtotal and My Buy Price.
-  var valuesSubtotal = sheet.getRange("A3:H4").getValues();
-  sheet.getRange(lastItemRow+1, 1, 2, 8).setValues(valuesSubtotal);
+  var subRange = sheet.getRange("A3:H4");
+  subRange.copyTo(sheet.getRange(lastItemRow+1, 1, 2, 8));
   
   // Find and cache the number of blank rows between first and second order.
   var blankCount = 0;
@@ -67,15 +67,19 @@ function priceSearch() {
   }
   
   // Move UPC and Notes information.
-  var upc = sheet.getRange(6, 6, itemCount).getValues();
+  var upcRange = sheet.getRange(6, 6, itemCount);
   var notes = sheet.getRange(7, 7, itemCount-1).getValues();
-  sheet.getRange(6, 4, itemCount).clear().setValues(upc);
+  upcRange.copyTo(sheet.getRange(6, 4, itemCount));
   sheet.getRange(7, 1, itemCount-1).setValues(notes);
   sheet.getRange(6, 5, itemCount).clear();
   
-  // Find and print sum of Count column.
+  // Find and print sum of Count and Amazon columns.
   var countA1 = sheet.getRange(6, 3, itemCount).getA1Notation();
+  var amazonA1 = sheet.getRange(6, 6, itemCount).getA1Notation();
+  var feesA1 = sheet.getRange(6, 7, itemCount).getA1Notation();
   sheet.getRange(lastItemRow+1, 3).setFormula("=SUM("+countA1+")");
+  sheet.getRange(lastItemRow+1, 6).setFormula("=SUM("+amazonA1+")");
+  sheet.getRange(lastItemRow+1, 7).setFormula("=SUM("+feesA1+")");
   
   // Update VLOOKUP range.
   var vlookupA1 = sheet.getRange(lastItemRow+6, 2, sheet.getLastRow()-lastItemRow-5, sheet.getLastColumn()-1).getA1Notation();
@@ -84,13 +88,12 @@ function priceSearch() {
   sheet.getRange(2, 7).setFormula("=VLOOKUP($B2,"+vlookupA1+",6,0)");
   
   // Copy A/E/R and Amazon formulas.
-  var formulas = sheet.getRange(2, 5, 1, 3).getValues();
+  var formulaRange = sheet.getRange(2, 5, 1, 3);
   for (i=0; i < itemCount; i++) {
-    for (j=0; j < formulas[0].length; j++) {
-      sheet.getRange(6+i, 5+j).setValue(formulas[0][j]);
-    }
+      formulaRange.copyTo(sheet.getRange(6+i, 5, 1, 3));
   }
   
   // Cache and paste values to overwrite the formulas.
-  
+  var vlookupValues = sheet.getRange(6, 5, itemCount, 3).getValues();
+  sheet.getRange(6, 5, itemCount, 3).setValues(vlookupValues);
 }
