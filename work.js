86f4f19462711b70897e6a0cbb40005d03ea45f7
Jon Avery <jon.avery92@gmail.com>
2017-05-22 11:47:21 -0700

    Add 'RHD' case to highlighting

--- work.js
+++ work.js
@@ -33,6 +33,30 @@ function getCol(matrix, col){
   return column;
 }
 
+function exportXML() {
+  var root = XmlService.createElement('items');
+  var items = SpreadsheetApp.getActiveSpreadsheet().getSheetByName('SCRAP').getDataRange().getValues();
+  for (i=1; i<items.length; i++) {
+    var child = XmlService.createElement('item')
+      .addContent(XmlService.createElement('SKU').setText(items[i][1]))
+      .addContent(XmlService.createElement('Title').setText(items[i][2]))
+      .addContent(XmlService.createElement('ASIN').setText(items[i][3]))
+      .addContent(XmlService.createElement('Condition').setText(items[i][15]))
+      .addContent(XmlService.createElement('Comment').setText(items[i][17]));
+    var grandchild = XmlService.createElement('dimensions')
+      .addContent(XmlService.createElement('Weight').setText(items[i][10]))
+      .addContent(XmlService.createElement('Length').setText(items[i][11]))
+      .addContent(XmlService.createElement('Width').setText(items[i][12]))
+      .addContent(XmlService.createElement('Height').setText(items[i][13]));
+    child.addContent(grandchild);
+    root.addContent(child);
+  }
+  var document = XmlService.createDocument(root);
+  var xml = XmlService.getPrettyFormat().format(document);
+  return xml;
+}
+
+
 function highlightAER() {
   /**
   * This script highlights each A/E/R cell according to its designation.
@@ -68,6 +92,9 @@ function highlightAER() {
         case 'd':
         case 'D':
           activeRange.setBackground('red');
+          break;
+        case 'RHD':
+          activeRange.setBackground('#00ffff');
           break;
         default:
           break;
@@ -163,12 +190,9 @@ function bulkUpdateLiquid() {
       var sku = parseInt(workSKU[i]);
       var workIndex = workSKU.indexOf(sku);
       var liquidIndex = liquidSKU.indexOf(sku);
-      Logger.log("SKU: " + sku)
-      Logger.log("Work: " + workIndex)
-      Logger.log("Liquid: " + liquidIndex)
     
       // Check if SKU is in liquidation sheet.
-      if (liquidIndex == -1) {notFound++; continue;}
+      if (liquidIndex == -1) {notFound++; Logger.log(sku); continue;}
       
       // Check if title is blank or already up to date.
       var workValues = sheetListings.getRange(workIndex+1, 3, 1, 3).getValues();
