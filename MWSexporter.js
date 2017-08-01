fefa59fb130eaee0ecaa9248eedc4a5257ba4f14
Jon Avery <jon.avery92@gmail.com>
2017-08-01 09:19:10 -0700

    Export data from MWS sheet

---
+++ MWSexporter.js
@@ -0,0 +1,28 @@
+function doGet() {
+  return getXML();
+}
+
+function getXML() {
+  var Items = XmlService.createElement('items');
+  var products = SpreadsheetApp.getActiveSpreadsheet().getSheetByName('MWS').getDataRange().getValues();
+  
+  for (var i = 1; i < products.length; i++) {
+    if(products[i][4] == "" || products[i][4] == "undefined") {continue;}
+    var Dimensions = XmlService.createElement('Dimensions')
+      .addContent(XmlService.createElement('Weight').setText(products[i][5]))
+      .addContent(XmlService.createElement('Length').setText(products[i][6]))
+      .addContent(XmlService.createElement('Width').setText(products[i][7]))
+      .addContent(XmlService.createElement('Height').setText(products[i][8]));
+    var Member = XmlService.createElement('Member')
+      .addContent(XmlService.createElement('SellerSKU').setText(products[i][0]))
+      .addContent(XmlService.createElement('Quantity').setText('1'))
+      .addContent(XmlService.createElement('ShipmentId').setText(products[i][11]))
+      .addContent(Dimensions);
+    Items.addContent(Member);
+  }
+  
+  var document = XmlService.createDocument(Items);
+  var xml = XmlService.getPrettyFormat().format(document);
+  Logger.log(xml)
+  return ContentService.createTextOutput(xml);
+}
