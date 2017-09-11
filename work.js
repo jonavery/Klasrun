d47483df11c777712fe42a3ab4e954a8243a8f9b
Jon <jon.avery92@gmail.com>
2017-09-11 09:07:16 -0700

    Fix unnamed variable error in bulkUpdate

--- work.js
+++ work.js
@@ -2,21 +2,20 @@ function onOpen() {
   var ui = SpreadsheetApp.getUi();
   ui.createMenu('Automation Menu')
     .addItem('Get Next SKU From Liquidation', 'newSKU')
-    .addItem('Update Item in Liquidation by SKU', 'updateBySKU')
     .addItem('Update All Work Items in Liquidation', 'bulkUpdateLiquid')
     .addSeparator()
     .addItem('Highlight Future Listings by A/E/R', 'highlightAER')
     .addSeparator()
-    .addSubMenu(ui.createMenu('Generate MWS item array')
-      .addItem('Standard Small Parcel', 'createMWS')
-      .addItem('Oversize (Palleted)', 'palletMWS')
+    .addSubMenu(ui.createMenu('Generate MWS item array')                                                                                      
+      .addItem('Standard Small Parcel', 'createMWS')                                                                                          
+      .addItem('Oversize (Palleted)', 'palletMWS')                                                                                            
       .addItem('Electronics', 'electronicsMWS'))
     .addItem('Populate MWS Tab', 'populateMWS')
     .addItem('Post Listings', 'postListings')
     .addSeparator()
     .addSubMenu(ui.createMenu('Create Shipments')
-      .addItem('Standard Small Parcel', 'createShipments')
-      .addItem('Oversize (Palleted)', 'shipLTL')
+      .addItem('Small Parcel', 'createShipments')
+      .addItem('LTL (Palleted)', 'shipLTL')
       .addItem('Electronics', 'shipElectronics'))
     .addToUi()
 }
@@ -223,65 +222,59 @@ function bulkUpdateLiquid() {
   var sheetListings = SpreadsheetApp.openById(workID).getSheetByName('Listings');
   var sheetLiquid = SpreadsheetApp.openById(liquidID).getSheetByName("Liquidation Orders");
   
-  // Prompt user for SKU.
-  var ui = SpreadsheetApp.getUi();
-  var response = ui.alert('This will update the liquidation sheet to match all items in the currently selected sheet.'
-                         +'\n\nHit OK to continue.\nClose this window to cancel.');
-  
-  if (response == ui.Button.OK) {
-    // Cache user-given SKU and initialize work and liquidation SKU's.
-    var workLastRow = sheetListings.getLastRow();
-    var workSKU = getCol(sheetListings.getRange(1, 2, workLastRow).getValues(), 0);
-    var workValues = sheetListings.getDataRange().getValues();
-    var liqLastRow = sheetLiquid.getLastRow();
-    var liquidSKU = getCol(sheetLiquid.getRange(1, 1, liqLastRow).getValues(), 0);
-    var liquidUPC = getCol(sheetLiquid.getRange(1, 5, liqLastRow).getValues(), 0);
+  // Cache user-given SKU and initialize work and liquidation SKU's.
+  var workLastRow = sheetListings.getLastRow();
+  var workSKU = getCol(sheetListings.getRange(1, 2, workLastRow).getValues(), 0);
+  var workValues = sheetListings.getDataRange().getValues();
+  var liqLastRow = sheetLiquid.getLastRow();
+  var liquidSKU = getCol(sheetLiquid.getRange(1, 1, liqLastRow).getValues(), 0);
+  var liquidUPC = getCol(sheetLiquid.getRange(1, 5, liqLastRow).getValues(), 0);
     
-    // Initialize counting variables.
-    var updated = 0;
-    var notUpdated = 0;
-    var created = 0;
+  // Initialize counting variables.
+  var updated = 0;
+  var notUpdated = 0;
+  var created = 0;
     
-    for (var i = 4; i < workSKU.length; i++) {
-      // Find index of SKU in work and liquidation.
-      var sku = parseInt(workSKU[i]);
-      var workIndex = workSKU.indexOf(sku);
-      var liquidIndex = liquidSKU.indexOf(sku);
+  for (var i = 4; i < workSKU.length; i++) {
+    // Find index of SKU in work and liquidation.
+    var sku = parseInt(workSKU[i]);
+    var workIndex = workSKU.indexOf(sku);
+    var liquidIndex = liquidSKU.indexOf(sku);
       
-      // Check if title is blank or already up to date.
-      if (workValues[i][2] == "" || workValues[i][3] == liquidUPC[liquidIndex]) {
-        notUpdated++;
-        continue;
-      }
+    // Check if title is blank or already up to date.
+    if (workValues[i][2] == "" || workValues[i][3] == liquidUPC[liquidIndex]) {
+      notUpdated++;
+      continue;
+    }
       
-     if (liquidIndex == -1) {
-       created++;
-       liquidIndex = liqLastRow;
-       var r = String(liquidIndex + 1);
-       sheetLiquid.insertRowAfter(liqLastRow);
+    if (liquidIndex == -1) {
+      created++;
+      liquidIndex = liqLastRow;
+      var r = String(liquidIndex + 1);
+      sheetLiquid.insertRowAfter(liqLastRow);
        
-       // Enter values from Work sheet.
-       sheetLiquid.getRange(r, 1).setValue(workValues[i][1]);
-       sheetLiquid.getRange(r, 2).setValue(todayDate());
-       sheetLiquid.getRange(r, 4).setValue("1");
-       sheetLiquid.getRange(r, 6).setValue("LIQUIDATION");
-       sheetLiquid.getRange(r, 8).setValue(workValues[i][6]);
+      // Enter values from Work sheet.
+      sheetLiquid.getRange(r, 1).setValue(workValues[i][1]);
+      sheetLiquid.getRange(r, 2).setValue(todayDate());
+      sheetLiquid.getRange(r, 4).setValue("1");
+      sheetLiquid.getRange(r, 6).setValue("LIQUIDATION");
+      sheetLiquid.getRange(r, 8).setValue(workValues[i][6]);
        
-       // Enter FBA information for new entry.
-       sheetLiquid.getRange(r, 9).setValue("FBA");             // Sell Site
-       sheetLiquid.getRange(r, 10).setValue("FBA");            // Sell Order
-       sheetLiquid.getRange(r, 11).setValue("0.01");           // Buy Price
+      // Enter FBA information for new entry.
+      sheetLiquid.getRange(r, 9).setValue("FBA");             // Sell Site
+      sheetLiquid.getRange(r, 10).setValue("FBA");            // Sell Order
+      sheetLiquid.getRange(r, 11).setValue("0.01");           // Buy Price
        
-       // Setup liquidation formulas for new entry.
-       sheetLiquid.getRange(r, 14).setFormula("=M"+r+"-K"+r);  // Actual Profit
-       sheetLiquid.getRange(r, 15).setFormula("=M"+r+"/K"+r);  // Actual % Increase
-       sheetLiquid.getRange(r, 22).setFormula("=VLOOKUP(A"+r+",Returns!A:A,1,0)");        // RETURNS V
-       sheetLiquid.getRange(r, 23).setFormula("=VLOOKUP(A"+r+",Salvage!A:A,1,0)");        // SALVAGE V
-       sheetLiquid.getRange(r, 24).setFormula("=VLOOKUP(A"+r+",Reimbursements!F:F,1,0)"); // REIMBURSE V
-       sheetLiquid.getRange(r, 25).setFormula("=VLOOKUP(A"+r+",Inventory!B:B,1,0)");      // INVENTORY V
+      // Setup liquidation formulas for new entry.
+      sheetLiquid.getRange(r, 14).setFormula("=M"+r+"-K"+r);  // Actual Profit
+      sheetLiquid.getRange(r, 15).setFormula("=M"+r+"/K"+r);  // Actual % Increase
+      sheetLiquid.getRange(r, 22).setFormula("=VLOOKUP(A"+r+",Returns!A:A,1,0)");        // RETURNS V
+      sheetLiquid.getRange(r, 23).setFormula("=VLOOKUP(A"+r+",Salvage!A:A,1,0)");        // SALVAGE V
+      sheetLiquid.getRange(r, 24).setFormula("=VLOOKUP(A"+r+",Reimbursements!F:F,1,0)"); // REIMBURSE V
+      sheetLiquid.getRange(r, 25).setFormula("=VLOOKUP(A"+r+",Inventory!B:B,1,0)");      // INVENTORY V
 //       sheetLiquid.getRange(liqLastRow + k, 26).setFormula("=VLOOKUP(A"+r+",Connor!G:H,2,0)");         // FBA SHIPMENT STATUS
 //       sheetLiquid.getRange(liqLastRow + k, 27).setFormula("=VLOOKUP(A"+r+",Connor!K:K,1,0)");         // FBA SHIPMENT ISSUE
-     }
+   }
       
     // Copy work information into liquidation.
     sheetLiquid.getRange(liquidIndex+1, 3).setValue(workValues[i][2]);
@@ -296,7 +289,6 @@ function bulkUpdateLiquid() {
       'Items updated: ' + updated
       + '\nItems already up to date: ' + notUpdated
       + '\nItems created: ' + created);
-  }
 }
 
 function createMWS() {
@@ -328,7 +320,7 @@ function palletMWS() {
   
   SpreadsheetApp.getUi().alert(
     'Go to the following URL and wait for a success message:\n\n'
-    + 'http://klasrun.com/AmazonMWS/MarketplaceWebServiceProducts/Functions/CreateItemArray.php');
+    + 'http://klasrun.com/AmazonMWS/MarketplaceWebServiceProducts/Functions/CreatePalletArray.php');
 }
 
 function electronicsMWS() {
@@ -345,69 +337,69 @@ function electronicsMWS() {
   
   SpreadsheetApp.getUi().alert(
     'Go to the following URL and wait for a success message:\n\n'
-    + 'http://klasrun.com/AmazonMWS/MarketplaceWebServiceProducts/Functions/CreateItemArray.php');
+    + 'http://klasrun.com/AmazonMWS/MarketplaceWebServiceProducts/Functions/CreateElectronicArray.php');
 }
 
 function populateMWS() {
-  /**
-  * This script accomplishes the following tasks:
-  *  1. Pull json file from MWS server
-  *  2. Convert json into multidimensional array
-  *  3. Push array into MWS tab.
-  */
-  
-  // Fetch the json array from website and parse into JS object.
-  var response = UrlFetchApp.fetch('http://klasrun.com/AmazonMWS/MarketplaceWebServiceProducts/Functions/MWS.json');
-  var json = response.getContentText();
-  var data = JSON.parse(json);
-  
-  // Convert data object into multidimensional array.
-  // Ordering is same as in MWS tab.
-  var itemCount = data.length;
-  var itemArray = makeArray(12, itemCount, "");
-  for (i = 0; i < itemCount; i++) {
-    var item = data[i];
-    itemArray[i] = ([
-      item.SellerSKU,
-      item.Title,
-      item.UPC,
-      item.ASIN,
-      item.Price,
-      item.Dimensions.Weight,
-      item.Dimensions.Length,
-      item.Dimensions.Width,
-      item.Dimensions.Height,
-      item.Condition,
-      item.Comment,
-      ""
-    ]);
-  }
-
-  // Push array into MWS tab.
-  var sheetMWS = SpreadsheetApp.getActiveSpreadsheet().getSheetByName('MWS');
-  var range = sheetMWS.getRange(2, 1, sheetMWS.getLastRow()-1, 12).clearContent().setBackground('white');
-  range.setValues(itemArray);
+   /**
+   * This script accomplishes the following tasks:
+   *  1. Pull json file from MWS server
+   *  2. Convert json into multidimensional array
+   *  3. Push array into MWS tab.
+   */
    
-  // Highlight undefined prices and wonky dimensions.
-  var prices = sheetMWS.getRange(2, 5, itemCount, 6).getValues();
-  for (i = 0; i < itemCount; i++) {
-    if (prices[i][0] == "undefined") {
-      sheetMWS.getRange(2+i, 5).setBackground('red');
-    }
-    if (Number(prices[i][1]) < 1 || Number(prices[i][1]) > 100) {
-      sheetMWS.getRange(2+i, 6).setBackground('red');
-    }
-    if (Number(prices[i][2]) < 1 || Number(prices[i][2]) > 75) {
-      sheetMWS.getRange(2+i, 7).setBackground('red');
-    }
-    if (Number(prices[i][3]) < 1 || Number(prices[i][3]) > 75) {
-      sheetMWS.getRange(2+i, 8).setBackground('red');
-    }
-    if (Number(prices[i][4]) < 1 || Number(prices[i][4]) > 75) {
-      sheetMWS.getRange(2+i, 9).setBackground('red');
-    }
-  }
-}
+   // Fetch the json array from website and parse into JS object.
+   var response = UrlFetchApp.fetch('http://klasrun.com/AmazonMWS/MarketplaceWebServiceProducts/Functions/MWS.json');
+   var json = response.getContentText();
+   var data = JSON.parse(json);
+   
+   // Convert data object into multidimensional array.
+   // Ordering is same as in MWS tab.
+   var itemCount = data.length;
+   var itemArray = makeArray(12, itemCount, "");
+   for (i = 0; i < itemCount; i++) {
+     var item = data[i];
+     itemArray[i] = ([
+       item.SellerSKU,
+       item.Title,
+       item.UPC,
+       item.ASIN,
+       item.Price,
+       item.Dimensions.Weight,
+       item.Dimensions.Length,
+       item.Dimensions.Width,
+       item.Dimensions.Height,
+       item.Condition,
+       item.Comment,
+       ""
+     ]);
+   }
+ 
+   // Push array into MWS tab.
+   var sheetMWS = SpreadsheetApp.getActiveSpreadsheet().getSheetByName('MWS');
+   var range = sheetMWS.getRange(2, 1, sheetMWS.getLastRow()-1, 12).clearContent().setBackground('white');
+   range.setValues(itemArray);
+   
+   // Highlight undefined prices and wonky dimensions.
+   var prices = sheetMWS.getRange(2, 5, itemCount, 6).getValues();
+   for (i = 0; i < itemCount; i++) {
+     if (prices[i][0] == "undefined") {
+       sheetMWS.getRange(2+i, 5).setBackground('red');
+     }
+     if (Number(prices[i][1]) < 1 || Number(prices[i][1]) > 100) {
+       sheetMWS.getRange(2+i, 6).setBackground('red');
+     }
+     if (Number(prices[i][2]) < 1 || Number(prices[i][2]) > 75) {
+       sheetMWS.getRange(2+i, 7).setBackground('red');
+     }
+     if (Number(prices[i][3]) < 1 || Number(prices[i][3]) > 75) {
+       sheetMWS.getRange(2+i, 8).setBackground('red');
+     }
+     if (Number(prices[i][4]) < 1 || Number(prices[i][4]) > 75) {
+       sheetMWS.getRange(2+i, 9).setBackground('red');
+     }
+   }
+ }
 
 function postListings() {
   /**
@@ -494,44 +486,3 @@ function importShipments() {
   }
 }
 
-function cancelListings() {
-  /**
-  * Run this script after postListings to cancel the feed submission.
-  *
-  * Only works before the feed has been processed by Amazon. Time is
-  * of the essence here!
-  */
-  
-  SpreadsheetApp.getUi().alert('ERROR: Script still in development.');
-  
-  // Call klasrun.com to cancel feed submission.
-  
-  
-  // Report status of cancellation.
-}
-
-function auditListings() {
-  /**
-  * This script accomplishes the following tasks:
-  *  1. Query amazon for listing status report.
-  *  2. Report status of three listing feeds.
-  *  3. When all three are complete, highlight items green in Scrap and MWS.
-  *  4. Migrate completed listings from Scrap to Archive.
-  */
-  
-  SpreadsheetApp.getUi().alert('ERROR: Script still in development.');
-  
-  // Grab status report from klasrun.com
-  
-  
-  // Broadcast status report via email.
-  
-  
-  // Initialize Scrap, MWS, and Archive sheets.
-  
-  
-  // Highlight complete items green in Scrap and MWS sheets.
-  
-  
-  // Move completed scrap entries into Archive sheet.
-}
